12f6bd32b8c1268f441f0f8d2c67857c84b22b35
TravisD42 <tdillard@carthage.edu>
2019-02-20 15:24:06 -0600

    Hecked it all up

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 //Change the name of the function to be called power and have it take an int base and an int exponent.
 // Print the base number raised to the exponent power.
-void power(int base, int exponent)
+void power(float base, int exponent)
 {
     cout << pow (base, exponent) << endl;
 }
 
 int main()
 {
-    power(2,3);
+    power(2.5, 3);
 
     return 0;
 }
